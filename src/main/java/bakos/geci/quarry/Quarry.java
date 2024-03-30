9de62860155008f045ace64fab748277b2ba075d
unknown <dudasdominik3@gmail.com>
2024-03-30 11:56:59 +0100

    init

---
+++ src/main/java/bakos/geci/quarry/Quarry.java
@@ -0,0 +1,17 @@
+package bakos.geci.quarry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Quarry extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
